b468f9fbed681791d24bc4ed879bd605ffee78a2
nutanfree <94290380+nutanfree@users.noreply.github.com>
2023-12-16 15:37:18 +0530

    Add files via upload

---
+++ enemy.js
@@ -0,0 +1,112 @@
+
+AFRAME.registerComponent("enemy-fireballs", {
+    init: function () {        
+        setInterval(this.shootEnemyMonster, 2000)
+    },
+    shootEnemyMonster: function () {
+        var scene = document.querySelector("#scene");
+
+        //enemyMonster entity
+        var enemyMonster = document.createElement("a-entity");
+
+
+
+
+
+
+
+        
+            
+        for (var i = 0; i < enemyMonster.length; i++) {
+
+       //create fireballs
+        var fireball = document.createElement("a-entity");
+
+        fireball.setAttribute("class","fireball")
+        fireball.setAttribute("gltf-model", "./models/fireball/scene.gltf");
+        fireball.setAttribute("dynamic-body", { mass: 0 });
+     
+        var pos=enemyMonster[i].getAttribute("position")
+
+        fireball.setAttribute("position", {
+            x: pos.x,
+            y: pos.y,
+            z: pos.z,
+        });
+        fireball.setAttribute("scale", {
+            x: 0.05,
+            y: 0.05,
+            z: 0.05,
+        });
+
+        scene.appendChild(fireball);      
+//Finding the direction vector using Three.js objects
+      var position1 = new THREE.Vector3();
+      var position2 = new THREE.Vector3();
+
+
+
+      var player = document.querySelector("#weapon").object3D
+      var enemy_fireball = fireball.object3D;
+      player.getWorldPosition(position1);
+      enemy_fireball.getWorldPosition(position2);
+
+      var direction = new THREE.Vector3();
+
+      direction.subVectors(position1, position2).normalize();
+
+      fireball.setAttribute("velocity", direction.multiplyScalar(20));
+
+
+
+
+
+        /******************************************************************************************* */
+
+        //check player life
+        
+
+
+        //collide event on enemy bullets
+        fireball.addEventListener("collide", function (e) {
+            if(e.detail.body.el.id === "weapon"){
+                if (playerLife > 0){
+                    playerLife -= 1;
+                    Element.setAttribute("text", {
+                        value: playerLife
+                    })
+                }
+            }   if (playerLife <= 0){
+
+
+                       //show text
+                  var txt = document.querySelector("#over");
+                  txt.setAttribute("visible", true);
+
+                    //remove monsters
+                    var monsterEl = document.querySelectorAll(".enemy")
+
+                    for ( var i = 0; i < monsterEl.length; i++) {
+                        scene.removeChild(tankEl[i])
+                    }
+
+
+
+
+
+            }
+           
+           
+
+                   
+
+                
+
+            
+        });
+
+    }
+    },
+    
+
+});
